fb7b302387e3a1a6f87a0e11b2390dbde290e8e7
phkim1993 <your@email.com>
2025-07-21 17:27:38 +0900

    super 과제 수정

--- 클래스연습/20250722_Super.py
+++ 클래스연습/20250722_Super.py
@@ -1,10 +1,9 @@
 class Animal:
     def __init__(self, name):
         self.name = name
-        print(f"내 이름은 {self.name}(이)야.")
 
     def speak(self):
-        print("안녕!")
+        print(f"내 이름은 {self.name}(이)야.")
 
 class Dog(Animal):
     def __init__(self, name):
@@ -14,5 +13,5 @@ class Dog(Animal):
         super().speak()
         print("멍!")
 
-d = Dog("비누")
+d = Dog("흰둥")
 d.speak()
